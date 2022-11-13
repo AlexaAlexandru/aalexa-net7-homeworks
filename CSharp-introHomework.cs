e84d221524e63f1814f60fbc767542332889da43
AlexaAlexandru <117767211+AlexaAlexandru@users.noreply.github.com>
2022-11-13 22:38:58 +0200

    updates on ex1

--- CSharp-introHomework.cs
+++ CSharp-introHomework.cs
@@ -1,6 +1,9 @@
 // exercise 1
-int num1 = 10;
-int num2 = 2;
+
+string input1 = Console.ReadLine();
+int num1 = Convert.ToInt32(input1);
+string input2 = Console.ReadLine();
+int num2 = Convert.ToInt32(input2);
 int addition = num1 + num2;
 int substraction = num1 - num2;
 int  division = num1 / num2;
@@ -14,9 +17,10 @@ Console.WriteLine("Division = " + division + " ");
 Console.WriteLine();
 Console.WriteLine("Multiplication = " + multiplication + " " );
 
-
-double num3 = 12.9;
-int num4 = 3;
+string input3 = Console.ReadLine();
+double num3 = Convert.ToDouble(input3);
+string input4 = Console.ReadLine();
+int num4 = Convert.ToInt32(input4);
 double add = num3 + num4;
 double sub = num3 - num4;
 double div = num3 / num4;
@@ -26,3 +30,10 @@ Console.WriteLine("Addition = " + add + " ");
 Console.WriteLine("Subtraction = " + sub + " ");
 Console.WriteLine("Division = " + div + " ");
 Console.WriteLine("Multiplication = " + mult + " ");
+
+// exercise 2
+
+int num5 = 33;
+int num6 = 11;
+int num7 = 25;
+
