aa3c46be552dd47a4a38ea0afb2c661db1643b0e
Alexa <117767211+AlexaAlexandru@users.noreply.github.com>
2022-11-13 22:13:59 +0200

    Update CSharp-introHomework.cs

--- CSharp-introHomework.cs
+++ CSharp-introHomework.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// exercise 1
+int num1 = 10;
+int num2 = 2;
+int addition = num1 + num2;
+int substraction = num1 - num2;
+int  division = num1 / num2;
+int multiplication = num1 * num2;
 
+Console.WriteLine("Addition = " + addition + " ");
+Console.WriteLine();
+Console.WriteLine("Subtraction = " + substraction + " ");
+Console.WriteLine();
+Console.WriteLine("Division = " + division + " ");
+Console.WriteLine();
+Console.WriteLine("Multiplication = " + multiplication + " " );
+
+
+double num3 = 12.9;
+int num4 = 3;
+double add = num3 + num4;
+double sub = num3 - num4;
+double div = num3 / num4;
+double mult = num3 * num4;
+
+Console.WriteLine("Addition = " + add + " ");
+Console.WriteLine("Subtraction = " + sub + " ");
+Console.WriteLine("Division = " + div + " ");
+Console.WriteLine("Multiplication = " + mult + " ");
