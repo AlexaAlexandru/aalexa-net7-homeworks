eb002dda4488901ac2e93730c378b802a0da2886
Alexa <117767211+AlexaAlexandru@users.noreply.github.com>
2022-11-15 13:40:24 +0200

    Exercise 7

--- CSharp-introHomework.cs
+++ CSharp-introHomework.cs
@@ -201,3 +201,55 @@ switch (grades)
 }
 
 Console.WriteLine(notes);
+
+//Exercise 7
+
+int num;
+string month;
+Console.WriteLine("Insert your grade : ");
+string input = Console.ReadLine();
+num = Convert.ToInt32(input);
+month = ".";
+switch (num)
+{
+    case 1 :
+        month = "January";
+        break;
+    case 2:
+        month = "February";
+        break;
+    case 3:
+        month = "March";
+        break;
+    case 4:
+        month = "April";
+        break;
+    case 5:
+        month = "May";
+        break;
+    case 6:
+        month = "June";
+        break;
+    case 7:
+        month = "July";
+        break;
+    case 8:
+        month = "August";
+        break;
+    case 9:
+        month = "September";
+        break;
+    case 10:
+        month = "Octomber";
+        break;
+    case 11:
+        month = "November";
+        break;
+    case 12:
+        month = "December";
+        break;
+
+}
+
+Console.WriteLine(month);
+
